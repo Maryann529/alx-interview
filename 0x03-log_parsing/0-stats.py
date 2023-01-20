eebfabd4e970c53702d4701a5343f2a8be5ab3d4
Maryann Unoarumi <maryann.pclng@gmail.com>
2023-01-20 06:29:26 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
-
+"""
+Log Parsing
+"""
 import sys
 
 
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+total_file_size = 0
+status = ['200', '301', '400', '401', '403', '404', '405', '500']
+obj = dict.fromkeys(status, 0)
 
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
+def printLogStat():
+    """
+    log stats
+    """
+    print("File size: {}".format(total_file_size))
+    for key, value in sorted(obj.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
 
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+if __name__ == "__main__":
+    count_stat = 0
+    try:
+        for line in sys.stdin:
+            line = line.split()
+            count_stat += 1
+            try:
+                total_file_size += int(line[-1])
+
+                if line[-2] in status:
+                    obj[line[-2]] += 1
+
+            except (IndexError, ValueError):
+                pass
+
+            if count_stat % 10 == 0:
+                printLogStat()
+    except KeyboardInterrupt:
+        printLogStat()
+        raise
+    else:
+        printLogStat()
